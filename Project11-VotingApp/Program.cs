bc4b1e55b603b5d8b8462fe5b184bb2b91b0439c
Arif Aydoğan <aydoganarif@yahoo.com>
2022-06-25 20:54:16 +0300

    Add project files.

---
+++ Project11-VotingApp/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Project11_VotingApp;
+
+Console.WriteLine("Lütfen kullanıcı adınızı giriniz: ");
+string user = Console.ReadLine().TrimEnd().TrimStart();
+
+bool log = Register.Login(user);
+if (!log)
+{
+    Console.WriteLine("\nKaydınız bulunamadı. Kaydınızı gerçekleştiriyoruz..");
+    Register reg = new Register(user);
+}
+Console.WriteLine("\nHoşgeldin {0}", user);
+Console.WriteLine("\nLütfen oy vereceğiniz seçeneği seçiniz," +
+    "\nSpor için 1," +
+    "\nMüzik için 2," +
+    "\nSinema için 3 yazınız.");
+
+Voting vot = new Voting(int.Parse(Console.ReadLine()));
+
+Console.WriteLine(Voting.getResult());
